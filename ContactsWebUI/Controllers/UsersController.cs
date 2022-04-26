2c59b4f06cd956668c27b9c1bc7a3583de5f6e47
Sefa <sefacabuk06@gmail.com>
2022-04-27 02:27:11 +0300

    kişi crud işlemleri tamamlandı..

--- ContactsWebUI/Controllers/UsersController.cs
+++ ContactsWebUI/Controllers/UsersController.cs
@@ -64,13 +64,74 @@ namespace ContactsWebUI.Controllers
         }
 
 
+        [Route("[controller]/[action]/{id}")]
+        public IActionResult Guncelle(string id)
+        {
+            UsersDto user = new UsersDto();
+
+            string link = Sabitler.ApiLink + "api/users/" + id;
+
+
+            try
+            {
+                var httpClient = new HttpClient();
+                
+                string json = "";
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                var postTask = httpClient.GetAsync(link);
+                postTask.Wait();
+                var postResult = postTask.Result;
+                var responJsonText = postResult.Content.ReadAsStringAsync().Result;
+
+                var sonuc = (JsonConvert.DeserializeObject<DataResult<UsersDto>>(responJsonText));
+
+                user = sonuc.Data;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return View(user);
+        }
+        
+        [Route("[controller]/[action]/{id}")]
+        public IActionResult Izle(string id)
+        {
+            UsersDto user = new UsersDto();
+
+            string link = Sabitler.ApiLink + "api/users/" + id;
+
+
+            try
+            {
+                var httpClient = new HttpClient();
+                
+                string json = "";
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                var postTask = httpClient.GetAsync(link);
+                postTask.Wait();
+                var postResult = postTask.Result;
+                var responJsonText = postResult.Content.ReadAsStringAsync().Result;
+
+                var sonuc = (JsonConvert.DeserializeObject<DataResult<UsersDto>>(responJsonText));
+
+                user = sonuc.Data;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return View(user);
+        }
+
         [HttpPost]
         public IActionResult KaydetYeni(UsersDto user)
         {
 
             string link = Sabitler.ApiLink + "api/users";
 
-            //object sonucList = new object();
 
             try
             {
@@ -93,7 +154,6 @@ namespace ContactsWebUI.Controllers
                     return Json(new { success = false, responseText = sonuc.Message });
                 }
 
-                //isverenIsgorenErisimHakkiView = sonuc.Data;
 
                 return Json(new { success = true, responseText = "Kayıt Başarılı" });
 
@@ -104,6 +164,82 @@ namespace ContactsWebUI.Controllers
             }
 
         }
+
+
+        [HttpPut]
+        public IActionResult KaydetGuncelle(UsersDto user)
+        {
+
+            string link = Sabitler.ApiLink + "api/users/";
+
+            try
+            {
+                var httpClient = new HttpClient();
+               
+                string json = "";
+                json = Newtonsoft.Json.JsonConvert.SerializeObject(user);
+                var httpContent = new StringContent(json, Encoding.UTF8, "application/json");
+                var postTask = httpClient.PutAsync(link, httpContent);
+                postTask.Wait();
+                var postResult = postTask.Result;
+                var responJsonText = postResult.Content.ReadAsStringAsync().Result;
+
+                var sonuc = (JsonConvert.DeserializeObject<DataResult<UsersDto>>(responJsonText));
+
+                if (!sonuc.Success)
+                {
+                    return Json(new { success = false, responseText = sonuc.Message });
+                }
+
+
+                return Json(new { success = true, responseText = "Guncelle Basarili" });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, responseText = "Guncelle Hata" });
+            }
+
+
+        }
+        
+        [HttpDelete]
+        public IActionResult KayitSil(string id)
+        {
+
+            string link = Sabitler.ApiLink + "api/users/"+ id ;
+
+            try
+            {
+                var httpClient = new HttpClient();
+               
+                string json = "";
+
+
+                var postTask = httpClient.DeleteAsync(link);
+                postTask.Wait();
+                var postResult = postTask.Result;
+                var responJsonText = postResult.Content.ReadAsStringAsync().Result;
+
+                var sonuc = (JsonConvert.DeserializeObject<DataResult<object>>(responJsonText));
+
+                if (!sonuc.Success)
+                {
+                    return Json(new { success = false, responseText = sonuc.Message });
+                }
+
+
+                return Json(new { success = true, responseText = "Sil Basarili" });
+
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, responseText = "Sil Hata" });
+            }
+
+
+        }
+
 
     }
 }
